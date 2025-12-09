6976d0a875e0e36d9299291472398ce045b92031
rladntjd85 <rladntjd850@gmail.com>
2025-12-09 10:36:02 +0900

    main chg

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,23 @@
 import Header from "./components/Header";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate, useLocation } from "react-router-dom";
+import { auth } from "./firebase";
 
 export default function App() {
+  const user = auth.currentUser;
+  const location = useLocation();
+
+  // 루트("/")로 접근했을 때만 리다이렉트 처리
+  if (location.pathname === "/") {
+    if (user) {
+      return <Navigate to="/board" replace />;
+    } else {
+      return <Navigate to="/login" replace />;
+    }
+  }
+
   return (
     <>
       <Header />
-
-      {/* 개별 페이지가 여기 안에 렌더됨 */}
       <div style={{ padding: "20px" }}>
         <Outlet />
       </div>
